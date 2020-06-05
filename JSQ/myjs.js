fa9595e34766a783d403caacef317174ecc9bf78
berkomeratay <57224724+berkomeratay@users.noreply.github.com>
2020-06-05 09:34:28 +0200

    Add files via upload

---
+++ JSQ/myjs.js
@@ -0,0 +1,91 @@
+
+$.getJSON("https://api.covid19api.com/summary",function(data){
+
+    
+
+
+        $("#btn3").click(function(){
+            let country_name = $("input").val();
+            console.log(country_name);
+
+            for (let index = 0; index < data.Countries.length; index++){
+                console.log(country_name + " " + data.Countries[index].Country);
+                if(country_name == data.Countries[index].Country ){
+                    
+                    let tr = data.Countries[index];
+                    let tr_new = tr.NewConfirmed;
+                    let tr_total = tr.TotalConfirmed;
+                    let tr_new_d = tr.NewDeaths;
+                    let tr_total_d = tr.TotalDeaths;
+                    let tr_new_rc = tr.NewRecovered;
+                    let tr_total_rc = tr.TotalRecovered;
+
+
+                    $(document).ready(function () {
+                        $("h1").text(tr.Country);
+                    });
+
+                    $(document).ready(function () {
+                        $("#new_con").text("New Cases: "+tr_new);
+                    });
+
+                    $(document).ready(function () {
+                        $("#total_con").text("Total Cases: "+tr_total);
+                    });
+
+                    $(document).ready(function () {
+                        $("#new_d").text("New Deaths: "+tr_new_d);
+                    });
+
+                    $(document).ready(function () {
+                        $("#total_d").text("Total Deaths: "+tr_total_d);
+                    });
+
+                    $(document).ready(function () {
+                        $("#new_rc").text("New Recovered: "+ tr_new_rc);
+                    });
+
+                    $(document).ready(function () {
+                        $("#total_rc").text("Total Recovered: "+tr_total_rc);
+                    });                    
+                    
+                    break;}
+            };
+            
+                
+                
+            });
+
+            
+    
+    
+    
+    
+       
+           
+           
+
+            
+             
+        
+        
+       
+           
+        
+
+        
+            
+       
+
+    
+    
+    
+    
+/*
+    data.Countries.forEach(land => {
+        console.log(land.Country)
+        
+    });
+*/   
+});
+
