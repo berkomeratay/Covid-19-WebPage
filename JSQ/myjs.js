e1b4197b8a6e00817a7fbcec7550e851bd1e6574
berkomeratay <57224724+berkomeratay@users.noreply.github.com>
2020-06-05 18:36:38 +0000

    Update myjs.js

--- JSQ/myjs.js
+++ JSQ/myjs.js
@@ -1,25 +1,35 @@
 $.getJSON("https://api.covid19api.com/summary",function(data){
 
+
+    
     for (let index_2 = 0; index_2 < data.Countries.length; index_2++) {
         var element = data.Countries[index_2].Country;
-        console.log("<option >"+element+"</option>");
+        //console.log("<option >"+element.toLowerCase().replace(" ","-")+"</option>");
         $("#country_selector").append("<option >"+element+"</option>");
         
         
     };
+
+
+
+    //https://api.covid19api.com/total/dayone/country/south-africa/status/confirmed
+
     
 
 
+
+
+    
+    
+
         $("#btn3").click(function(){
             let country_name = $("#country_selector").val();
-            console.log(country_name);
+            console.log(country_name.toLowerCase().replace(" ","-"));
 
             
 
-
-
             for (let index = 0; index < data.Countries.length; index++){
-                console.log(country_name + " " + data.Countries[index].Country);
+                //console.log(country_name + " " + data.Countries[index].Country);
                 if(country_name == data.Countries[index].Country ){
                     
                     let tr = data.Countries[index];
@@ -57,7 +67,57 @@ $.getJSON("https://api.covid19api.com/summary",function(data){
 
                     $(document).ready(function () {
                         $("#total_rc").text("Total Recovered: "+tr_total_rc);
-                    });                    
+                    });  
+                    
+                    $.getJSON("https://api.covid19api.com/total/dayone/country/"+country_name.toLowerCase().replace(" ","-") +"/status/confirmed",function(sec_data){
+
+                        let xs = [];
+                        let ys = [];
+                        for (let index = 0; index < sec_data.length; index++) {
+                            const element = sec_data[index].Cases;
+                            ys.push(element);
+                            xs.push(index+1)
+                            
+                        };
+
+                        //console.log(xs);
+
+                        //console.log("this world: "+sec_data[0].Cases);
+                        let myCanvas = document.getElementById("myCanvas").getContext("2d");
+
+                    
+                    var myChart = new Chart(myCanvas, {
+                        type: 'line',
+                        data: {
+                            labels: xs,
+                            datasets: [{
+                                label: '# of Votes',
+                                data: ys,
+                                backgroundColor: "rgb(47, 72, 234)",
+                                borderColor: "rgb(47, 72, 234)",
+                                    
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            responsive: false,
+                            maintainAspectRatio: false,
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
+                        }
+                    });
+
+                    });
+                    
+        
+                    
+        
+        
                     
                     break;}
             };
@@ -67,3 +127,8 @@ $.getJSON("https://api.covid19api.com/summary",function(data){
             });
            
 });
+
+
+
+
+
